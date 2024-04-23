e9163df68e68a3a6ebec384f0be8d3af58474bd0
Matan.Bardugo <matanew1@gmail.com>
2024-04-24 02:51:12 +0300

    fix bugs

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -1,22 +1,16 @@
 // middleware.js
-import cors from "cors"; // apply to all requests for Cross-Origin Resource Sharing (CORS) support
-import express from "express"; // apply to all requests for parsing incoming request bodies
-import compression from 'compression'; // apply to all requests for compressing responses with gzip/deflate
-import helmet from 'helmet'; // apply to all requests for security headers and other security protections
-import rateLimit from "express-rate-limit"; // apply to all requests to limit repeated requests to public APIs and/or endpoints
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
+import cors from "cors";
+import express from "express";
+import compression from 'compression';
+import helmet from 'helmet';
 
 const setupMiddleware = (app) => {
-  app.use(cors()); // apply to all requests
-  app.use(express.json()); // apply to all requests
-  app.use(express.urlencoded({ extended: true })); // apply to all requests
-  app.use(compression()); // apply to all requests
-  app.use(helmet()); // apply to all requests for security headers and other security protections
-  app.use(limiter); // apply to all requests
+  app.set('trust proxy', 1); // Add this line
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(compression());
+  app.use(helmet());
 };
 
 export default setupMiddleware;
