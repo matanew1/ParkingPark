39cc3c3b9eb3f67b89f92563d7e6786aec59e2de
Matan.Bardugo <matanew1@gmail.com>
2024-04-24 13:15:47 +0300

    xenove

--- server/AIModel/model.js
+++ server/AIModel/model.js
@@ -1,36 +1,20 @@
 // textGenerationService.mjs
 import fetch from "isomorphic-fetch";
 import { pipeline } from "@xenova/transformers";
-import { env } from '@xenova/transformers';
-import { createRequire } from "module";
-import {dirname} from 'path';
-
-
-
-env.allowRemoteModels = false;
-env.allowLocalModels = true;
-env.allowCache = true;
-env.localModelPath = dirname(import.meta.url).replace('%D7%9E%D7%AA%D7%9F', 'מתן').replace('file:///', '');
-// env.localModelPath = "C:/Users/מתן/Desktop/ParkingPark/server/AIModel/";
-env.cacheDir = "./cache";
 
 let decisionMaker;
 
 console.log("Loading models...");
-
-
 const loadModels = async () => {
   try {
-    const decisionMakerModel = await pipeline("text2text-generation", "LaMini-Flan-T5-783M");
+    const [decisionMakerModel] = await Promise.all([
+      pipeline("text2text-generation", "Xenova/LaMini-Flan-T5-783M"),
+    ]);
     decisionMaker = decisionMakerModel;
-
-    console.log("Models loaded successfully");
   } catch (error) {
     console.error("Error loading models:", error);
   }
 };
-
-
 loadModels().then(() => {
   console.log("---------------Models loaded----------------");
 });
@@ -39,11 +23,7 @@ loadModels().then(() => {
 const decisionMakerByText = async (text) => {
   try {
     if (!decisionMaker) {
-      console.log("Decision maker not loaded. Loading now...");
-      await loadModels();
-      if (!decisionMaker) {
-        throw new Error("Failed to load decision maker");
-      }
+      throw new Error("Decision maker not loaded");
     }
 
     const decision = await decisionMaker(text, {
